1ae98e26fdfde18f1d7e16ccf3a0d4f2b86179b7
rajat <irajatmishra13@gmail.com>
2020-05-19 00:52:43 +0530

    writerpad day1 II

---
+++ src/main/java/com/xebia/writerpad/WriterpadApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.writerpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xebia/writerpad/WriterpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebia.writerpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
